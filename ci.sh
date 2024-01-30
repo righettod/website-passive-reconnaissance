4e960e28d0f93f09305ac617e851c255fdc0e6af
drighetto <drighetto@excellium-services.com>
2024-01-30 10:43:04 +0100

    Add CI script

--- ci.sh
+++ ci.sh
@@ -4,12 +4,12 @@
 ############################################################
 # Define the output file destination
 OUT=/tmp/out-$RANDOM-$RANDOM.tmp
-echo "OUT set to file: $OUT."
+echo "[+] OUT set to file: $OUT."
 # Execute the script
 python wpr.py -d righettod.eu -s -n 8.8.8.8 -t 30 > $OUT
 # Validate the execution
 marker=$(grep -Fc "Reconnaissance finished" $OUT)
-echo "Marker occurences found into the OUT file: $marker."
+echo "[+] Marker occurences found into the OUT file: $marker."
 rm $OUT
 if [ $marker -eq 0 ]
 then
