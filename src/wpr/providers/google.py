839e61c6b53bdb013ebd6ccad5f1ef930251ebe5
righettod <dominique.righetto@gmail.com>
2026-04-12 19:30:37 +0200

    try to fix the issue with google dorks.

--- src/wpr/providers/google.py
+++ src/wpr/providers/google.py
@@ -2,6 +2,7 @@
 Provider to perform google dork via the "googlesearch-python" package.
 """
 
+import requests
 from googlesearch import search
 
 from wpr.common import OSINTProvider, OSINTProviderData
@@ -17,11 +18,20 @@ class Google(OSINTProvider):
         super().__init__(name="GoogleDorks", target_ip_or_domain=ip_or_domain)
 
     def call(self, req_timeout: int = DEFAULT_CALL_TIMEOUT) -> OSINTProviderData:
-        dorks = []
-        dorks.append(f'site:pastebin.com "{self.target_ip_or_domain}"')
-        dorks.append(" OR filetype:".join(INTERESTING_FILE_EXTENSIONS))
-        dorks.append(RCE_PRONE_PARAMETERS_DORK % self.target_ip_or_domain)
+        file_types = " OR filetype:".join(INTERESTING_FILE_EXTENSIONS)
+        dorks = {}
+        dorks["PasteBin"] = f'site:pastebin.com "{self.target_ip_or_domain}"'
+        dorks["FileTypes"] = f"site:{self.target_ip_or_domain} filetype:{file_types}"
+        dorks["RCE"] = RCE_PRONE_PARAMETERS_DORK % self.target_ip_or_domain
         information_lines = {}
-        for dork in dorks:
-            information_lines[dork] = list(search(term=dork, sleep_interval=5, num_results=100, timeout=req_timeout, safe=""))
+        for dork_name, dork_expr in dorks.items():
+            try:
+                data = list(search(dork_expr, num_results=100, lang="en", timeout=req_timeout))
+                if len(data) == 0:
+                    data.append("Query found nothing but try manually the following dork from a browser:")
+                    data.append(dork_expr)
+                information_lines[dork_name] = data
+            except requests.exceptions.HTTPError as err:
+                if err.response.status_code == 429:
+                    information_lines[dork_name] = ["HTTP 429 received so manual call required!", f"\nDork expression => {dork_expr}", f"\nError message   => {str(err)}\n"]
         return OSINTProviderData(information_lines=information_lines, description_of_data_type="Google Dorks for the domain")
