7b1dd1ea2cef627c931eee0234b4e1e3616aec6e
righettod <dominique.righetto@gmail.com>
2026-04-16 14:56:05 +0200

    Fix build error for #166.

--- src/wpr/constants.py
+++ src/wpr/constants.py
@@ -1,7 +1,7 @@
 """
 Contains constants and configuration items. All time units are in seconds.
 """
-import os
+import shutil
 
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 MOBILE_APP_STORE_COUNTRY_STORE_CODE = "LU"  # Luxembourg
@@ -9,4 +9,4 @@ DEFAULT_CALL_TIMEOUT = 240
 HEADER_LENGTH = 50
 MAX_CALL_TENTATIVES = 4
 RETRY_WAIT_DELAY = 4
-TERMINAL_WIDTH = os.get_terminal_size().columns
+TERMINAL_WIDTH = shutil.get_terminal_size().columns
